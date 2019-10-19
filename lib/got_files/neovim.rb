5e3bf818bfba66041930ca69bbb5744c0a0254cb
Stephen Hoekstra <stephenhoekstra@gmail.com>
2019-10-19 10:51:19 +0200

    Update NeoVim install
    Signed-off-by: Stephen Hoekstra <stephenhoekstra@gmail.com>

--- lib/got_files/neovim.rb
+++ lib/got_files/neovim.rb
@@ -4,12 +4,21 @@ module GotFiles
   class Neovim < Base
 
     def install
+      puts 'Checking for dependencies...'
+
+      brew_install_if_missing('cmake', 'CMake')
+      brew_install_if_missing('go', 'Golang')
+      brew_install_if_missing('python', 'Python 3')
+
       brew_install_if_missing('neovim', 'Neovim')
 
       system("pip3 install --upgrade pynvim --user", :out => File::NULL)
+      system("pip3 install --upgrade neovim --user", :out => File::NULL)
     end
 
     def setup
+      system('defaults write com.googlecode.iterm2 "Draw Powerline Glyphs" -bool true')
+
       mkdir_p File.expand_path("~/.config/nvim/backup")
       mkdir_p File.expand_path("~/.config/nvim/cache")
       mkdir_p File.expand_path("~/.config/nvim/undo")
@@ -17,10 +26,10 @@ module GotFiles
       File.open(File.expand_path("~/.config/nvim/init.vim"), 'w') do |file|
         file.write(
           [
-	    "\" Add all neovim configuration to the file sourced below, this file is not managed",
+            "\" Add all neovim configuration to the file sourced below, this file is not managed",
             "source ~/.nvimrc\n"
-	  ].join("\n")
-	)
+          ].join("\n")
+        )
       end
 
       install_file 'files/neovim/nvimrc'
