5ff108bfb14eb62c6aef2b66c0207a60caf99cc9
Stephen Hoekstra <stephenhoekstra@gmail.com>
2020-08-20 23:07:12 +0200

    Merge zsh and terminal class/task
    Signed-off-by: Stephen Hoekstra <stephenhoekstra@gmail.com>

--- lib/got_files/terminal.rb
+++ lib/got_files/terminal.rb
@@ -6,18 +6,27 @@ module GotFiles
     def install
       brew_cask_install_if_missing('iterm2', 'iTerm2')
 
+      brew_install_if_missing('coreutils') # required gdircolors and gls
+      brew_install_if_missing('ccat', 'ccat')
       brew_install_if_missing('rename', 'rename')
-
-      brew_install_if_missing('fzf', 'fzf')
-      system('$(brew --prefix)/opt/fzf/install --key-bindings --no-update-rc --no-bash --no-fish --completion', :out => File::NULL)
 
       # Git tools
       brew_install_if_missing('hub', 'hub')
       brew_install_if_missing('github/gh/gh', 'GitHub CLI')
       brew_install_if_missing('zaquestion/tap/lab', 'GitLab CLI')
+
+      install_fzf
+      install_zim
     end
 
     def setup
+      # Use gnu dircolors and ls
+      %w(dircolors ls).each do |f|
+        symlink("/usr/local/bin/g#{f}",
+                "/usr/local/bin/#{f}",
+                force: true)
+      end
+
       # Open the app so the preference files get initialized unless iTerm2 is already running
       unless ENV['TERM_PROGRAM'] == 'iTerm.app'
         system('open -g "/Applications/iTerm.app" && sleep 2 && killall iTerm2')
@@ -36,6 +45,22 @@ module GotFiles
       system %(killall cfprefsd)
 
       puts 'Restart iTerm2 using ⌘+Q to settings to take effect...'
+    end
+
+    private
+
+    def install_fzf
+      brew_install_if_missing('fzf', 'fzf')
+
+      system('$(brew --prefix)/opt/fzf/install --key-bindings --no-update-rc --no-bash --no-fish --completion', :out => File::NULL)
+    end
+
+    def install_zim
+      system('curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh')
+
+      install_file Dir.glob('files/zsh/*')
+
+      system('zsh ~/.zim/zimfw.zsh install')
     end
   end
 end
