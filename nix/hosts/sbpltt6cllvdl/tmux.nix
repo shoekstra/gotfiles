fd0a6017826a70404f248b499415c05c0ec7fe63
Stephen Hoekstra <stephenhoekstra@gmail.com>
2023-05-18 16:09:52 +0200

    Update Oh my tmux! to latest version and fix tmux error
    Signed-off-by: Stephen Hoekstra <stephenhoekstra@gmail.com>

--- nix/hosts/sbpltt6cllvdl/tmux.nix
+++ nix/hosts/sbpltt6cllvdl/tmux.nix
@@ -7,14 +7,13 @@
   ];
 
   home.file = {
-    ".tmux".source = pkgs.fetchFromGitHub {
+    ".tmux.conf".source = pkgs.fetchFromGitHub {
        owner = "gpakosz";
        repo = ".tmux";
        rev = "7706ab724f3811479a358c6f9ea6aeb6decece5f";
        sha256 = "sha256-o126SathIT49Mj9EZDhlYF5i3EgRe2HFZkoGXTVQBbw=";
-    };
+    } + "/.tmux.conf";
 
-    ".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "/Users/shoekstra/.tmux/.tmux.conf";
     ".tmux.conf.local".source = ../../../files/tmux/tmux.conf.local;
   };
 }
