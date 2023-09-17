3066274605e4f95171e2f365774389c6db9f4f22
Stephen Hoekstra <stephenhoekstra@gmail.com>
2023-09-17 23:47:24 +0200

    Drop "Oh my tmux!" and configure something more minimal
    Kept the essentials configured via nix and dumped the .tmux repo and my
    local overrides.
    
    Added the One Half tmux theme and set some essential key bindings.
    
    Signed-off-by: Stephen Hoekstra <stephenhoekstra@gmail.com>

--- nix/users/shoekstra/tmux.nix
+++ nix/users/shoekstra/tmux.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }:
 
-{
+let
+  tmux-onehalf-theme = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tmux-onehalf-theme";
+    rtpFilePath = "main.tmux";
+    version = "master";
+    src = pkgs.fetchFromGitHub {
+      owner = "andersondanilo";
+      repo = "tmux-onehalf-theme";
+      rev = "1a099d775c948bc1b72c16c05aca5fb2f1bd3c03";
+      sha256 = "sha256-ExUJc14niGSNzqX3e1GjxZMv0y4gLmzfoQG0hzRFI5s=";
+    };
+  };
+
+in {
   home.packages = with pkgs; [
     reattach-to-user-namespace
   ];
@@ -17,8 +30,27 @@
 
     extraConfig = ''
       # Switch to last window, screen style
-      bind-key a send-prefix
-      bind-key C-a last-window
+      bind a send-prefix
+      bind C-a last-window
+
+      # Split current window and use cwd
+      unbind %
+      unbind '"'
+      bind _ split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+
+      # Window navigation
+      unbind n
+      unbind p
+      bind -r C-h previous-window
+      bind -r C-l next-window
+      bind Tab last-window
+
+      # Synchronise panes
+      bind S set-window-option synchronize-panes
+
+      # Reload config file
+      bind r source ~/.config/tmux/tmux.conf \; display "tmux config sourced!"
 
       # Apply true color
       set-option -ga terminal-overrides ",xterm-256color:Tc"
@@ -35,6 +67,11 @@
           sha256 = "sha256-sw9g1Yzmv2fdZFLJSGhx1tatQ+TtjDYNZI5uny0+5Hg=";
         };
       }))
+      tmuxPlugins.better-mouse-mode
+      tmuxPlugins.cpu
+      {
+        plugin = tmux-onehalf-theme; # This wants to be loaded last
+      }
     ];
   };
 }
